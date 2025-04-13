84f4f08afb19814b80f97b34093361a3ef77b825
glocaldm <glocal.dm@outlook.com>
2025-04-13 11:53:57 +0000

    03

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import json
 
 @functions_framework.http
 def fetch_and_store_data(request):
+     # Allow cross-origin requests
+    allowed_origins = ["https:/glocaldm.github.io"]
+    origin = request.headers.get("Origin")
+
+    headers = {
+        "Access-Control-Allow-Origin": origin if origin in allowed_origins else "",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Max-Age": "3600"
+    }
 
     # URL of the external resource
     external_url = "https://ical.booking.com/v1/export?t=219b9892-bf77-4998-b804-30590502eaf6"
